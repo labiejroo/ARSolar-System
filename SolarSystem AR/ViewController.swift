d5d73195f9bfb22d9d9862f81f8b37474156553e
Machine Horizon <cudoskylab@machines-macbook-pro.local>
2018-02-27 13:04:32 +0100

    Initial Commit

---
+++ SolarSystem AR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SolarSystem AR
+//
+//  Created by Machine Horizon on 27/02/2018.
+//  Copyright © 2018 Machine Horizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
