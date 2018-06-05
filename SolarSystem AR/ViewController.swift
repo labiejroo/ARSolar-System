33dad25dd76f700a11f0352a2c91034011677ff4
Machine Horizon <>
2018-06-05 21:02:50 +0200

    Init

--- SolarSystem AR/ViewController.swift
+++ SolarSystem AR/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import ARKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin,ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.autoenablesDefaultLighting = true
+        self.sceneView.session.run(configuration)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        let earth = SCNNode(geometry: SCNSphere(radius: 0.2))
+        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "2k_earth_daymap (1)")
+        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "2k_earth_specular_map")
+        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "2k_earth_clouds")
+        earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "2k_earth_normal_map")
+        earth.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(earth)
+        
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        earth.runAction(forever)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +43,10 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension Int {
+    
+    var degreesToRadians: Double { return Double(self) * .pi/180}
 }
 
